d81ffeee7682e7faf7b26941bf849d268e96a485
VCD <VeryCrazyDog@gmail.com>
2009-09-22 12:56:47 +0000

    lf2crlf.c + Added MSN protocol checking

--- lf2crlf/lf2crlf.c
+++ lf2crlf/lf2crlf.c
@@ -28,10 +28,12 @@
 #include <m_skin.h>
 #include <m_database.h>
 #include <m_system.h>
+#include <m_protocols.h>
 
 // GUID = {8BBC5C4A-1131-4488-8120-4144422AEB7E}
 #define MIID_LF2CRLF { 0x8bbc5c4a, 0x1131, 0x4488, { 0x81, 0x20, 0x41, 0x44, 0x42, 0x2a, 0xeb, 0x7e } }
 #define LF2CRLF_SERVICE "LF2CRLF/Convert"
+#define MSN_PROTO_NAME "MSN"
 
 HINSTANCE hInst;
 PLUGINLINK *pluginLink;
@@ -50,6 +52,13 @@ PLUGININFOEX pluginInfo={
 	0,
 	MIID_LF2CRLF
 };
+
+static BOOL isBelongProtocol(HANDLE hContact, char *protocol) {	
+	char *accBaseName;
+
+	accBaseName = (char *)CallService(MS_PROTO_GETCONTACTBASEACCOUNT, (WPARAM)hContact, 0);
+	return strcmp(accBaseName, MSN_PROTO_NAME) == 0;
+}
 
 static unsigned int countLF(PBYTE buf, DWORD bufSize) {
 	unsigned int result = 0;
@@ -94,92 +103,94 @@ static unsigned int doConvert() {
 	hContact = (HANDLE)CallService(MS_DB_CONTACT_FINDFIRST, 0, 0);
 	// TODO: Check if the contact is MSN contact or not
 	while (hContact != NULL) {
-		DBEVENTINFO dbei;
-		HANDLE hDbEvent;
-		int currentBlobSize;
+		if(isBelongProtocol(hContact, MSN_PROTO_NAME)) {
+			DBEVENTINFO dbei;
+			HANDLE hDbEvent;
+			int currentBlobSize;
 
-		ZeroMemory(&dbei, sizeof(dbei));
-		dbei.cbSize = sizeof(dbei);
-		dbei.pBlob = NULL;
-		hDbEvent = (HANDLE)CallService(MS_DB_EVENT_FINDFIRST, (WPARAM)hContact, 0);
-		currentBlobSize = 0;
-		while (hDbEvent != NULL) {
-			int msgLen;
-			int neededBlobSize;
-			unsigned int lfCount;
-			BOOL doneReplace = FALSE;
+			ZeroMemory(&dbei, sizeof(dbei));
+			dbei.cbSize = sizeof(dbei);
+			dbei.pBlob = NULL;
+			hDbEvent = (HANDLE)CallService(MS_DB_EVENT_FINDFIRST, (WPARAM)hContact, 0);
+			currentBlobSize = 0;
+			while (hDbEvent != NULL) {
+				int msgLen;
+				int neededBlobSize;
+				unsigned int lfCount;
+				BOOL doneReplace = FALSE;
 
-			// Check if the current size of the blob is large enough or not
-			msgLen = CallService(MS_DB_EVENT_GETBLOBSIZE,(WPARAM)hDbEvent,0);
-			neededBlobSize = msgLen;
-			if(neededBlobSize > currentBlobSize) {
-				dbei.pBlob = (PBYTE)mir_realloc(dbei.pBlob, neededBlobSize);
-				currentBlobSize = neededBlobSize;
-			}
-			dbei.cbBlob = currentBlobSize;
-			// Get the message event
-			CallService(MS_DB_EVENT_GET, (WPARAM)hDbEvent, (LPARAM)&dbei);
-			// Count the number of LF
-			lfCount = countLF(dbei.pBlob, dbei.cbBlob);
-			// Prepare to replace LF by CRLF if there is any LF in the event
-			if(lfCount > 0) {
-				// Calculate and obtain the new size needed
-				neededBlobSize = msgLen + lfCount;
+				// Check if the current size of the blob is large enough or not
+				msgLen = CallService(MS_DB_EVENT_GETBLOBSIZE,(WPARAM)hDbEvent,0);
+				neededBlobSize = msgLen;
 				if(neededBlobSize > currentBlobSize) {
 					dbei.pBlob = (PBYTE)mir_realloc(dbei.pBlob, neededBlobSize);
 					currentBlobSize = neededBlobSize;
-					// Get the message again since the memory has been reallocated
-					CallService(MS_DB_EVENT_GET, (WPARAM)hDbEvent, (LPARAM)&dbei);
 				}
-				// Prepare the size of the converted message
-				dbei.cbBlob = neededBlobSize;
-				// Replace LF with CRLF
-				replaceLFbyCRLF(dbei.pBlob, msgLen, lfCount);
-				//// Trim CRLF at the end
-				//i = dbei.cbBlob - 2;
-				//while(i > 0) {
-				//	if(dbei.pBlob[i] == '\n' && dbei.pBlob[i - 1] == '\r') {
-				//		dbei.pBlob[i - 1] = '\0';
-				//		dbei.cbBlob = dbei.cbBlob - 2;
-				//		i = i - 2;
-				//	}
-				//	else if(dbei.pBlob[i] == ' ') {
-				//		dbei.pBlob[i] = '\0';
-				//		dbei.cbBlob = dbei.cbBlob - 1;
-				//		i = i - 1;
-				//	}
-				//	else {
-				//		i = 0;
-				//	}
-				//}
-				//// Trim one CRLF if any
-				//if(currentBlobSize > 3) {
-				//	if(dbei.pBlob[currentBlobSize - 2] == '\n' && dbei.pBlob[currentBlobSize - 2 - 1] == '\r') {
-				//		dbei.pBlob[currentBlobSize - 2 - 1] = '\0';
-				//		dbei.cbBlob = dbei.cbBlob - 2;
-				//	}
-				//}
-				{
-					HANDLE hEventToDelete = hDbEvent;
+				dbei.cbBlob = currentBlobSize;
+				// Get the message event
+				CallService(MS_DB_EVENT_GET, (WPARAM)hDbEvent, (LPARAM)&dbei);
+				// Count the number of LF
+				lfCount = countLF(dbei.pBlob, dbei.cbBlob);
+				// Prepare to replace LF by CRLF if there is any LF in the event
+				if(lfCount > 0) {
+					// Calculate and obtain the new size needed
+					neededBlobSize = msgLen + lfCount;
+					if(neededBlobSize > currentBlobSize) {
+						dbei.pBlob = (PBYTE)mir_realloc(dbei.pBlob, neededBlobSize);
+						currentBlobSize = neededBlobSize;
+						// Get the message again since the memory has been reallocated
+						CallService(MS_DB_EVENT_GET, (WPARAM)hDbEvent, (LPARAM)&dbei);
+					}
+					// Prepare the size of the converted message
+					dbei.cbBlob = neededBlobSize;
+					// Replace LF with CRLF
+					replaceLFbyCRLF(dbei.pBlob, msgLen, lfCount);
+					//// Trim CRLF at the end
+					//i = dbei.cbBlob - 2;
+					//while(i > 0) {
+					//	if(dbei.pBlob[i] == '\n' && dbei.pBlob[i - 1] == '\r') {
+					//		dbei.pBlob[i - 1] = '\0';
+					//		dbei.cbBlob = dbei.cbBlob - 2;
+					//		i = i - 2;
+					//	}
+					//	else if(dbei.pBlob[i] == ' ') {
+					//		dbei.pBlob[i] = '\0';
+					//		dbei.cbBlob = dbei.cbBlob - 1;
+					//		i = i - 1;
+					//	}
+					//	else {
+					//		i = 0;
+					//	}
+					//}
+					//// Trim one CRLF if any
+					//if(currentBlobSize > 3) {
+					//	if(dbei.pBlob[currentBlobSize - 2] == '\n' && dbei.pBlob[currentBlobSize - 2 - 1] == '\r') {
+					//		dbei.pBlob[currentBlobSize - 2 - 1] = '\0';
+					//		dbei.cbBlob = dbei.cbBlob - 2;
+					//	}
+					//}
+					{
+						HANDLE hEventToDelete = hDbEvent;
 
-					// Find the next message first
+						// Find the next message first
+						hDbEvent = (HANDLE)CallService(MS_DB_EVENT_FINDNEXT, (WPARAM)hDbEvent, 0);
+						// Delete the old message
+						CallService(MS_DB_EVENT_DELETE, (WPARAM)hContact, (LPARAM)hEventToDelete);
+						// Add the converted one
+						CallService(MS_DB_EVENT_ADD, (WPARAM)hContact, (LPARAM)&dbei);
+						// Add the counter
+						convertedEvent++;
+						// Set the flag
+						doneReplace = TRUE;
+					}
+				}
+				if(doneReplace == FALSE) {
 					hDbEvent = (HANDLE)CallService(MS_DB_EVENT_FINDNEXT, (WPARAM)hDbEvent, 0);
-					// Delete the old message
-					CallService(MS_DB_EVENT_DELETE, (WPARAM)hContact, (LPARAM)hEventToDelete);
-					// Add the converted one
-					CallService(MS_DB_EVENT_ADD, (WPARAM)hContact, (LPARAM)&dbei);
-					// Add the counter
-					convertedEvent++;
-					// Set the flag
-					doneReplace = TRUE;
 				}
 			}
-			if(doneReplace == FALSE) {
-				hDbEvent = (HANDLE)CallService(MS_DB_EVENT_FINDNEXT, (WPARAM)hDbEvent, 0);
+			if(dbei.pBlob != NULL) {
+				mir_free(dbei.pBlob);
 			}
-		}
-		if(dbei.pBlob != NULL) {
-			mir_free(dbei.pBlob);
 		}
 		hContact = (HANDLE)CallService(MS_DB_CONTACT_FINDNEXT, (WPARAM)hContact, 0);
 	}
